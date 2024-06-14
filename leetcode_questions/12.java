59447c6a10f671ba4b2349772d43536912a05e9a
params19 <paramsrivastava9@gmail.com>
2024-06-14 23:42:57 +0530

    integer to roman

---
+++ leetcode_questions/12.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String intToRoman(int num) {
+         int[] valus = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanNumeral = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder s = new StringBuilder();
+
+        int i = 0;
+    
+        while (num > 0) {
+            if (num >= valus[i]) {
+                
+                s.append(romanNumeral[i]);
+                num -= valus[i];
+            } else {
+                i++;
+            }
+        }
+
+        return s.toString();
+    }
+}
