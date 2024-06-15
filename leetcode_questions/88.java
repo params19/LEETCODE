d59b2d2a320cff783391ce1bb536d7a9c52895e6
params19 <paramsrivastava9@gmail.com>
2024-06-15 13:55:18 +0530

    merge sorted array

---
+++ leetcode_questions/88.java
@@ -0,0 +1,23 @@
+class Solution {
+    //Time complexity : O(n)
+    //Space complexity : O(1)
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=m-1,j=n-1,k=m+n-1;
+        while(j>=0)
+        {
+            if(i>=0 && nums2[j]<nums1[i])
+            {
+                nums1[k]=nums1[i];
+                i--;
+                k--;
+            }
+            else
+            {
+                nums1[k]=nums2[j];
+                j--;
+                k--;
+            }
+        }
+        
+    }
+}
