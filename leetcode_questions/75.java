f7757cc4d8e6d8cbdbffeb0ff26be39c121233b3
params19 <paramsrivastava9@gmail.com>
2024-06-16 13:15:54 +0530

    sort colors

---
+++ leetcode_questions/75.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int l=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+            l=Math.max(l,nums[i]);
+        }
+        int c[]=new int[l+1];
+        for(int i=0;i<nums.length;i++)
+        {
+            c[nums[i]]++;
+        }
+        int j=0;
+        for(int i=0;i<c.length;i++)
+        {
+            while(c[i]>0)
+            {
+                nums[j]=i;
+                j++;
+                c[i]--;
+            }
+        }
+    }
+}
