04f083d07cb9af0055b3e9c807c4550cbbe7445d
params19 <paramsrivastava9@gmail.com>
2024-06-30 23:39:10 +0530

    count pairs that form the complete day

---
+++ leetcode_contest/3142.java
@@ -0,0 +1,17 @@
+package leetcode_contest;
+
+class Solution {
+    public int countCompleteDayPairs(int[] hours) {
+    int[] freq = new int[24]; // Array to store frequency of hours (0-23)
+
+    int ans = 0; // Stores the final count of complete day pairs
+
+    for (int hour : hours) 
+    {
+      int req = (24 - hour % 24) % 24; // Calculate complement for a complete day
+      ans += freq[req]; // Add count of elements that can form a complete day with current hour
+      freq[hour%24]++; // Increment frequency of current hour
+    }
+    return ans; 
+  }
+}
