90f775ff9cef0fd2b1a28f293eb7f344cd1b9b71
params19 <paramsrivastava9@gmail.com>
2024-07-03 19:29:21 +0530

    minimum difference between largest and smallest

---
+++ leetcode_questions/1509.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minDifference(int[] nums) {
+        int n = nums.length;
+        if (n <= 4) {
+            return 0; 
+        }
+        Arrays.sort(nums);  
+        int minDiff = Integer.MAX_VALUE;
+        for (int i = 0; i <= 3; i++) 
+        {
+            minDiff = Math.min(minDiff, nums[n - 4 + i] - nums[i]);
+        }
+        return minDiff;
+    }
+}
