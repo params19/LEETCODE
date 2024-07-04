fa2424e2f2758b46320401e5f67bba51927ae375
params19 <paramsrivastava9@gmail.com>
2024-07-04 19:47:47 +0530

    sum between zeros

--- leetcode_questions/qr.java
+++ leetcode_questions/qr.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Solution {
+public class qr {
     public long maximumImportance(int n, int[][] roads) {
         long freq[]=new long[n];
         int n1=roads.length;
