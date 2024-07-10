e354b434ad1689117d4c1c6075c2d446500a5e65
params19 <paramsrivastava9@gmail.com>
2024-07-10 20:04:53 +0530

    max points on a straigth line brute force

---
+++ max_points_on_line.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxPoints(int[][] points) {
+        int n=points.length;
+        if(n==1)
+        return 1;
+        int res=0;
+
+        for(int i=0;i<n;i++)//Point 1(x1,y1)
+        {
+            for(int j=i+1;j<n;j++)//Point 2(x2,y2)
+            {
+                int cnt=2;
+                int dx=points[j][0]-points[i][0];//(x2-x1)
+                int dy=points[j][1]-points[i][1];//(y2-y1)
+
+                for(int k=0;k<n;k++)
+                {
+                    if(k!=i && k!=j)
+                    {
+                    int dx1=points[k][0]-points[i][0];//(xk-x1)
+                    int dy1=points[k][1]-points[i][1];//(yk-y1)
+
+                    if(dx*dy1==dy*dx1)//(x2-x1)*(yk-y1)==(y2-y1)*(xk-x1)
+                    {
+                        cnt++;
+                    }
+                }
+            }
+                res=Math.max(res,cnt);
+            }
+        }
+        return res;
+    }
+}
