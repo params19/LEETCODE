fd8f4399334c95d325b971cce73854df42cc60cd
params19 <paramsrivastava9@gmail.com>
2024-07-11 10:12:31 +0530

    reverse substring between each parethesis

---
+++ reverse_substrings.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+class Solution {
+    public String reverseParentheses(String s) {
+        int n=s.length();
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<n;i++)
+        {
+            if(s.charAt(i)!=')')
+            {
+                stack.push(s.charAt(i));
+            }
+            else
+            {
+                StringBuilder sb=new StringBuilder();
+                while(stack.peek()!='(')
+                {
+                    sb.append(stack.pop());
+                }
+                stack.pop();
+                for(int j=0;j<sb.length();j++)
+                {
+                    stack.push(sb.charAt(j));
+                }
+            }
+        }
+        StringBuilder ans=new StringBuilder();
+        while(!stack.isEmpty())
+        {
+            ans.append(stack.pop());
+        }
+        return ans.reverse().toString();
+    }
+}
