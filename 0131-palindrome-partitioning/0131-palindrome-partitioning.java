caed3283f4a0df45f30793f94bb14979f4ee2c60
Param Srivastava <144816241+params19@users.noreply.github.com>
2024-08-14 23:31:03 +0530

    Time: 8 ms (56.09%), Space: 57.3 MB (9.10%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,34 +1,32 @@
 import java.util.*;
 class Solution {
-    public static List < List < String >> partition(String s) {
-        List < List < String >> res = new ArrayList < > ();
-        List < String > path = new ArrayList < > ();
-        partitionHelper(0, s, path, res);
-        return res;
+    public List<List<String>> partition(String s) {
+        List<List<String>> res=new ArrayList<>();
+        List<String> temp=new ArrayList<>();
+        int n=s.length();
+        return rec(0,n,s,res,temp);
     }
-
-    static void partitionHelper(int index, String s, List < String > path, List < List < String >> res) {
-        if (index == s.length()) 
-        {
-            res.add(new ArrayList < > (path));
-            return;
+    private List<List<String>> rec(int i,int n,String s,List<List<String>> res,List<String> temp){
+        if(i==n){
+            res.add(new ArrayList<>(temp));
+            return res;
         }
-        for (int i = index; i < s.length(); ++i) 
-        {
-            if (isPalindrome(s, index, i)) 
-            {
-                path.add(s.substring(index, i + 1));
-                partitionHelper(i + 1, s, path, res);
-                path.remove(path.size() - 1);
+        for(int j=i;j<n;j++){
+            if(isPalin(s,i,j)){
+                temp.add(s.substring(i,j+1));
+                rec(j+1,n,s,res,temp);
+                temp.remove(temp.size()-1);
             }
         }
+        return res;
     }
-
-    static boolean isPalindrome(String s, int start, int end) {
-        while (start <= end)
-        {
-            if (s.charAt(start++) != s.charAt(end--))
-            return false;
+    private boolean isPalin(String s,int i,int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
         }
         return true;
     }
