849301d3bccade6b4c66ba70ff1df9b28c6b68cc
Param Srivastava <144816241+params19@users.noreply.github.com>
2024-08-24 20:22:44 +0530

    Time: 2 ms (99.25%), Space: 43.5 MB (11.85%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,36 +1,28 @@
 import java.util.*;
 class Solution {
-    private void finalcomb(int ind,int[] candidates,int target,List<List<Integer>> ans,List<Integer> ds)
-    {
-        if(target == 0)
-        {
-            ans.add(new ArrayList<>(ds));
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        Arrays.sort(candidates);  // 
+        solve(0, candidates, target, res, temp);
+        return res;
+    }
+
+    private void solve(int idx, int[] candidates, int target, List<List<Integer>> res, List<Integer> temp) {
+        if (target == 0) {
+            res.add(new ArrayList<>(temp));  
             return;
         }
-        for(int i=ind;i<candidates.length;i++)
-        {
-            if(i>ind && candidates[i] == candidates[i-1])
-            //here i>ind is used bcz we dont want to consider the same element again
-            //and candidates[i] == candidates[i-1] is used to avoid duplicates
-            {
-                continue;
+        for (int i = idx; i < candidates.length; i++) {
+            if (i > idx && candidates[i] == candidates[i - 1]) {
+                continue; 
             }
-            if(candidates[i] > target)
-            {
-                break;
+            if (candidates[i] > target) {
+                break; 
             }
-            ds.add(candidates[i]);
-            finalcomb(i+1,candidates,target-candidates[i],ans,ds);
-            //i+1 is used bcz we dont want to consider the same element again
-            ds.remove(ds.size()-1);
+            temp.add(candidates[i]);
+            solve(i + 1, candidates, target - candidates[i], res, temp); 
+            temp.remove(temp.size() - 1);
         }
-    }
-    
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        
-        List<List<Integer>> ans=new ArrayList<>();
-        Arrays.sort(candidates);
-        finalcomb(0,candidates,target,ans,new ArrayList<>());
-        return ans;
     }
 }
