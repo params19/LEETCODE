bd46c77b07512179664b4bf31e23107f77094d94
Param Srivastava <144816241+params19@users.noreply.github.com>
2024-08-24 23:59:42 +0530

    Time: 1 ms (86.34%), Space: 43.1 MB (62.84%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+   
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        solve(0, 0, new StringBuilder(), n * 2, res);
+        return res;
+    }
+
+    private void solve(int i, int open, StringBuilder sb, int n, List<String> res) {
+        if (i == n) {
+            if (open == 0) {
+                res.add(sb.toString());
+            }
+            return;
+        }
+
+        sb.append('(');
+        solve(i + 1, open + 1, sb, n, res);
+        sb.deleteCharAt(sb.length() - 1);
+
+        if (open > 0) {
+            sb.append(')');
+            solve(i + 1, open - 1, sb, n, res);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
