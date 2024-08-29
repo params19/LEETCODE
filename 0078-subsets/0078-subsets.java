d4a5f221b7e818c155f486b6561c772781f98dbe
Param Srivastava <144816241+params19@users.noreply.github.com>
2024-08-29 18:12:20 +0530

    Time: 1 ms (52.25%), Space: 42.5 MB (85.04%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,26 +1,39 @@
-import java.util.*;
+// import java.util.*;
+// class Solution {
+//     public List<List<Integer>> result = new ArrayList<>(); 
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<Integer> temp = new ArrayList<>(); 
+//         solve(nums, 0, temp); 
+//         return result; 
+//     }
+//     private void solve(int[] nums, int idx, List<Integer> temp) {
+//         if (idx >= nums.length)
+//         { 
+//             result.add(new ArrayList<>(temp)); 
+//             return;
+//         }
+//         temp.add(nums[idx]);
+//         solve(nums, idx + 1, temp);
+//         temp.remove(temp.size() - 1);
+//         solve(nums, idx + 1, temp);
+//     }
+// }
 class Solution {
-    private List<List<Integer>> result = new ArrayList<>(); // Holds all subsets
-
     public List<List<Integer>> subsets(int[] nums) {
-        List<Integer> temp = new ArrayList<>(); // Temporary list to build subsets
-        solve(nums, 0, temp); // Start backtracking from index 0
-        return result; // Return the final list of subsets
-    }
-
-    private void solve(int[] nums, int idx, List<Integer> temp) {
-        if (idx >= nums.length)
-        { // Base case: if index is out of bounds
-            result.add(new ArrayList<>(temp)); // Add the current subset to the result list
-            return;
+        List<List<Integer>> res=new ArrayList<>();
+        int n=nums.length;
+        for(int mask=0;mask<(1<<nums.length);mask++)
+        {
+            List<Integer> ll=new ArrayList<>();
+            for(int i=0;i<nums.length;i++)
+            {
+                if((mask&(1<<i))!=0)
+                {
+                    ll.add(nums[i]);
+                }
+            }
+            res.add(ll);
         }
-
-        // Include nums[idx] in the current subset and move to the next element
-        temp.add(nums[idx]);
-        solve(nums, idx + 1, temp);
-
-        // Backtrack: remove the last element and explore the possibility of not including nums[idx]
-        temp.remove(temp.size() - 1);
-        solve(nums, idx + 1, temp);
+        return res;
     }
 }
