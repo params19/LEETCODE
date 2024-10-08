622ef039452085b76f402a4962727de7ad60265e
Param Srivastava <144816241+params19@users.noreply.github.com>
2024-10-08 08:18:23 +0530

    Time: 7 ms (7.30%), Space: 42.3 MB (40.24%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int n=s.length();
+        String ans="";
+        int[] last_ndex_cnt=new int[26];
+        boolean[] visited=new boolean[26];
+        for(int i=0;i<n;i++){
+            last_ndex_cnt[s.charAt(i)-'a']=i;
+        }
+
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            int idx=ch-'a';
+            if(visited[idx]){
+                continue;
+            }
+            while(ans.length()>0 && ans.charAt(ans.length()-1)>ch && last_ndex_cnt[ans.charAt(ans.length()-1)-'a']>i){
+                visited[ans.charAt(ans.length()-1)-'a']=false;
+                ans=ans.substring(0,ans.length()-1);
+            }
+            ans+=ch;
+            visited[idx]=true;
+        }
+        return ans;
+    }
+}
