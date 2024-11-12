b3496d648dcd32a72e3f8efe87a326b5220ca026
Param Srivastava <144816241+params19@users.noreply.github.com>
2024-11-11 23:52:58 -0800

    Time: 82 ms (7.25%), Space: 79.5 MB (91.12%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.java
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.java
@@ -1,31 +1,28 @@
-import java.util.Arrays;
-import java.util.TreeMap;
-
 class Solution {
     public int[] maximumBeauty(int[][] items, int[] qq) {
-        int q = qq.length;
-        int n = items.length;
-        int[] ans = new int[q];
-
+        int q=qq.length;
+        int n=items.length;
+        int m=items[0].length;
+        int ans[]=new int[q];
         Arrays.sort(items, (a, b) -> Integer.compare(a[0], b[0]));
-
-        TreeMap<Integer, Integer> mp = new TreeMap<>();
-        int maxBeauty = 0;
+        TreeMap<Integer, Integer> mp=new TreeMap<>();
+        int maxBeauty=0;
         for (int[] item : items) {
             int price = item[0];
             int beauty = item[1];
+
             maxBeauty = Math.max(maxBeauty, beauty);
-            mp.put(price, maxBeauty); 
+            mp.put(price, maxBeauty);
         }
+
         for (int i = 0; i < q; i++) {
             Integer closestPrice = mp.floorKey(qq[i]);
             if (closestPrice != null) {
                 ans[i] = mp.get(closestPrice);
             } else {
-                ans[i] = 0;
+                ans[i] = 0; 
             }
         }
-        
         return ans;
     }
 }
