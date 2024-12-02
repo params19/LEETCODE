0934f5895abc3169de4b0e3cdfc26899d78936a9
Param Srivastava <144816241+params19@users.noreply.github.com>
2024-12-02 00:03:48 -0800

    Time: 0 ms (100.00%), Space: 41.5 MB (27.01%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -1,22 +1,22 @@
 class Solution {
     public int isPrefixOfWord(String s, String st) {
         int n = s.length();
-        String check=""; 
+        StringBuilder check = new StringBuilder(); 
         int cnt = 0;
-        for (int i = 0; i < n; i++) 
-        {
+        
+        for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-            if (ch != ' ') 
-            {
-                check+=ch; 
+            if (ch != ' ') {
+                check.append(ch);
             } else {
                 cnt++;
-                if (check.startsWith(st)) return cnt;
-                check="";
+                if (check.toString().startsWith(st)) return cnt;
+                check.setLength(0);
             }
         }
         cnt++;
-        if (check.startsWith(st)) return cnt;
+        if (check.toString().startsWith(st)) return cnt;
+
         return -1;
     }
 }
