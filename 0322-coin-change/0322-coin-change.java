e3acdd83838467f47cc349aa2ad139f1b50ea91e
Param Srivastava <144816241+params19@users.noreply.github.com>
2025-06-30 23:09:38 +0530

    Time: 37 ms (13.75%), Space: 46 MB (6.9%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        int ans = solve(0, amount, coins, dp);
+        return ans == (int)1e9 ? -1 : ans;
+    }
+    static int solve(int i, int amt, int[] coins, int[][] dp) {
+        if (i == coins.length) {
+            return amt == 0 ? 0 : (int)1e9; 
+        }
+        if (dp[i][amt] != -1) return dp[i][amt];
+        int notTake = solve(i + 1, amt, coins, dp);
+        int take = (int)1e9;
+        if (coins[i] <= amt) {
+            take = 1 + solve(i, amt - coins[i], coins, dp);
+        }
+        return dp[i][amt] = Math.min(take, notTake);
+    }
+}
