1714e7817a707c9ee14fc144d74d2429111c5cda
Param Srivastava <144816241+params19@users.noreply.github.com>
2025-07-04 15:33:21 +0530

    Time: 3 ms (42.24%), Space: 45.5 MB (10.56%) - LeetHub

---
+++ 0979-di-string-match/0979-di-string-match.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n=s.length();
+        int arr[]=new int [n+1];
+        Arrays.fill(arr, -1);
+        int inc=0, dec=n;
+        for(int i=0;i<n;i++)
+        {
+            if(s.charAt(i)=='I')
+            {
+                arr[i]=inc;
+                inc++;
+            }
+            else{
+                arr[i]=dec;
+                dec--;
+            }
+        }
+        arr[n]=inc;
+        return arr;
+    }
+}
